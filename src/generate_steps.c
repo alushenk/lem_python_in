5bab835e946d78430616450cdd9917aa91f54ebf
duandetected <wartoxy@gmail.com>
2017-08-03 19:21:26 +0300

    little optimization

--- src/generate_steps.c
+++ src/generate_steps.c
@@ -48,79 +48,91 @@ void	create_steps(t_path *path, int start_name, t_room *end)
 		add_step(path, step);
 		i++;
 	}
-
 }
 
-t_step	*combine_steps(t_path *list, int paths_count)
+void		toggle_next_step(t_path	*path, int len)
 {
-	t_path	*path;
-	t_step	*step;
 	t_step	*temp;
+
+	while(len > 0)
+	{
+		temp = path->steps;
+		if (temp != NULL)
+		{
+			path->steps = temp->next;
+		}
+		free(temp);
+		path->ended = 0;
+		path = path->next;
+		len--;
+	}
+}
+
+int	pop_move(t_path *list, t_step *step, int ended)
+{
 	t_move	*move;
-	t_step	*result;
-	int		empty;
+
+	move = list->steps->move;
+	list->steps->move = move->next;
+	if (move->next == NULL)
+	{
+		list->ended = 1;
+		ended += 1;
+	}
+	move->next = NULL;
+	add_move(step, move);
+	return (ended);
+}
+
+void	combine_steps(t_step **result, t_path *list, int paths_count, int empty)
+{
+	t_step	*step;
 	int 	ended;
 
-	empty = 0;
-	result = NULL;
 	while(empty < paths_count)
 	{
 		empty = 0;
-		step = create_step();
 		ended = 0;
+		step = create_step();
 		while(empty < paths_count && ended < paths_count)
 		{
-			path = list;
-			while(path)
+			list = list->next;
+			if (list->steps == NULL && list->ended == 0)
 			{
-				if (path->steps && path->steps->move)
-				{
-					move = path->steps->move;
-					path->steps->move = move->next;
-					if (move->next == NULL)
-					{
-						path->ended = 1;
-						ended += 1;
-					}
-					move->next = NULL;
-					add_move(step, move);
-				}
-				else
-					if (path->ended == 0)
-						empty += 1;
-				path = path->next;
-			}
-		}
-		append_step(&result, step);
-		path = list;
-		while(path)
-		{
-			temp = path->steps;
-			if (temp == NULL)
+				list->ended = 1;
 				empty += 1;
-			else
-				path->steps = temp->next;
-			free(temp);
-			path->ended = 0;
-
-			path = path->next;
+				ended += 1;
+				continue;
+			}
+			if (list->ended == 1)
+				continue;
+			ended = pop_move(list, step, ended);
 		}
+		append_step(result, step);
+		toggle_next_step(list, paths_count);
 	}
-	return (result);
 }
 
 void	generate_steps(t_graph *graph)
 {
 	t_path	*path;
+	int 	num;
 	int		i;
 
 	i = 1;
 	path = graph->chosen_group->paths;
-	while(path)
+	while(1)
 	{
 		create_steps(path, i, graph->end_room);
 		i += path->ants_count;
+		if (path->next == NULL)
+		{
+			path->next = graph->chosen_group->paths;
+			break ;
+		}
 		path = path->next;
 	}
-	graph->steps = combine_steps(graph->chosen_group->paths, graph->chosen_group->number_of_paths);
+	path = graph->chosen_group->paths;
+	num = graph->chosen_group->number_of_paths;
+	combine_steps(&graph->steps, path, num, 0);
 }
