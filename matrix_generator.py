1b4e6cdc781d1cd90f28e99c8129e20d71b42e2f
duandetected <wartoxy@gmail.com>
2017-08-24 23:35:12 +0300

    added love

--- matrix_generator.py
+++ matrix_generator.py
@@ -2,7 +2,7 @@
 import sys
 import os
 
-def generate_matrix(file, x, y):
+def generate_matrix(file, x, y, start_name, end_name):
     matrix = []
     i = 0
     for a in range(y):
@@ -19,19 +19,35 @@ def generate_matrix(file, x, y):
                 connections.append((matrix[a][b][0], matrix[a][b + 1][0]))
             if a + 1 < y:
                 connections.append((matrix[a][b][0], matrix[a + 1][b][0]))
+            if b + 1 < x and a + 1 < y:
+            	connections.append((matrix[a][b][0], matrix[a + 1][b + 1][0]))
+            	connections.append((matrix[a + 1][b][0], matrix[a][b + 1][0]))
 
-    file.write('20\n##start\n')
+    if end_name == 0:
+    	end_name = matrix[-1][-1][0]
+    file.write('20\n')
     for i, line in enumerate(matrix):
         for j, elem in enumerate(line):
-            if i + 1 == y and j + 1 == x:
-                file.write('##end\n')
-            file.write('{} {} {}\n'.format(*elem))
+        	if elem[0] == start_name:
+        		file.write('##start\n')
+        	if elem[0] == end_name:
+        		file.write('##end\n')
+        	file.write('{} {} {}\n'.format(*elem))
 
     for line in connections:
         file.write('{}-{}\n'.format(*line))
 
 if __name__ == "__main__":
     length = len(sys.argv)
+    start_name = 0
+    end_name = 0
+    if length == 6:
+    	start_name = int(sys.argv[4])
+    	end_name = int(sys.argv[5])
+    	length -= 2
+    if length == 5:
+    	start_name = int(sys.argv[4])
+    	length -= 1
     if length == 4:
         path = sys.argv[1]
         basedir = os.path.dirname(path)
@@ -40,6 +56,6 @@ if __name__ == "__main__":
         with open(path, 'w') as file:
             x = int(sys.argv[2])
             y = int(sys.argv[3])
-            generate_matrix(file, x, y)
+            generate_matrix(file, x, y, start_name, end_name)
     else:
-        print("usage: [output file name] [x] [y]")
+        print("usage: [output file name] [x] [y] [start name - optional] [end name - optional]")
